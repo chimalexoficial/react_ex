eafc51f4a7461dbc1d80b5b3c94ffb1737d80456
chimalexoficial <you@example.com>
2023-06-19 19:19:07 -0500

    Multi state commented

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
-    const [title, setTitle] = useState(props.title);
-    console.log('ExpenseItem evaluated by React')
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-    }
-
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button>Change Title</button>
             </div>
-            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,13 +1,10 @@
-import './newExpense.css'
+import './NewExpense.css'
+import ExpenseForm from './ExpenseForm'
 
-const newExpense = () => {
-
-    return <div>
-        <form>
-
-        </form>
+const NewExpense = () => {
+    return <div className='new-expense'>
+        <ExpenseForm />
     </div>
-
 }
 
-export default newExpense;
+export default NewExpense;
