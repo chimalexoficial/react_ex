f63fa74848773ece549f41357b2bef8ee45cf1d3
chimalexoficial <you@example.com>
2023-06-23 15:47:21 -0700

    two way binding, clearing states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -41,21 +41,37 @@ const ExpenseForm = () => {
         // })
     }
 
-    return <form>
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+
+    }
+
+    return <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' onChange={titleChageHandler} />
+                <input type='text' onChange={titleChageHandler} value={enteredTitle} />
             </div>
 
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input onChange={amountChangeHandler} type='number' min='0.01' step='0.01' />
+                <input onChange={amountChangeHandler} value={enteredAmount} type='number' min='0.01' step='0.01' />
             </div>
 
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input onChange={dateChangeHandler} type='date' min='2019-01-01' max='2022-12-31' />
+                <input onChange={dateChangeHandler} value={enteredDate} type='date' min='2019-01-01' max='2022-12-31' />
             </div>
         </div>
         <div className='new-expense__actions'>
